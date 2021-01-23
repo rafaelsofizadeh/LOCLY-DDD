5701e3c4cf25a852e5a66ac5c38d011ac47a9d9f
rafaelsofizadeh <rafa.sofizadeh@gmail.com>
2021-01-23 23:20:55 +0400

    Mapper using class-validator instead of manual serialization

--- src/order/infrastructure/repository/order/OrderMongoMapper.ts
+++ src/order/infrastructure/repository/order/OrderMongoMapper.ts
@@ -1,14 +1,17 @@
 import { Binary } from 'mongodb';
 import * as MUUID from 'uuid-mongodb';
+import { classToPlain } from 'class-transformer';
 
-import { Item, ItemProps } from '../../../domain/entity/Item';
-import { Order, OrderStatus, ShipmentCost } from '../../../domain/entity/Order';
-import { Address, AddressProps } from '../../../domain/entity/Address';
-import { UniqueEntityID } from '../../../../common/domain/UniqueEntityId';
+import { AddressProps } from '../../../domain/entity/Address';
+import { ItemProps } from '../../../domain/entity/Item';
+import {
+  OrderProps,
+  OrderStatus,
+  ShipmentCost,
+} from '../../../domain/entity/Order';
 
 export type OrderMongoDocument = {
   _id: Binary;
-  // https://devblogs.microsoft.com/typescript/announcing-typescript-4-1/#template-literal-types
   status: OrderStatus;
   customerId: Binary;
   originCountry: string;
@@ -17,43 +20,18 @@ export type OrderMongoDocument = {
   destination: AddressProps;
 };
 
-export function orderToMongoDocument({
-  id,
-  status,
-  customer: { id: customerId },
-  originCountry,
-  items,
-  shipmentCost,
-  destination,
-}: Order): OrderMongoDocument {
-  return {
-    _id: uniqueEntityIdToMongoId(id),
-    customerId: uniqueEntityIdToMongoId(customerId),
-    status,
-    originCountry,
-    items: items.map(item => serializeItem(item)),
-    shipmentCost,
-    destination: serializeAddress(destination),
+export function orderToMongoDocument(order: OrderProps): OrderMongoDocument {
+  // For id, see: Entity { @Transform() id }
+  const { id: rawId, ...restPlainOrder } = classToPlain(order) as Omit<
+    OrderMongoDocument,
+    '_id'
+  > & {
+    id: string;
   };
-}
+  const mongoBinaryId = MUUID.from(rawId);
 
-function uniqueEntityIdToMongoId(id: UniqueEntityID) {
-  console.log(id, id.toValue());
-  return MUUID.from(id.toValue());
-}
-
-function serializeAddress(address: Address) {
-  return { country: address.country };
-}
-
-function serializeItem(item: Item) {
   return {
-    title: item.title,
-    storeName: item.storeName,
-    category: item.category,
-    weight: item.weight,
-    width: item.width,
-    length: item.length,
-    height: item.height,
+    _id: mongoBinaryId,
+    ...restPlainOrder,
   };
 }
