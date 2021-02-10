fda65ed59e633e58ab3ed515cabcfb02ae2cee90
rafaelsofizadeh <rafa.sofizadeh@gmail.com>
2021-02-10 15:57:52 +0400

    Hot module reload (doesn't work due to webpack issues)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './AppModule';
 
+// TODO(GLOBAL): https://github.com/microsoft/TypeScript/issues/2845
+
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
