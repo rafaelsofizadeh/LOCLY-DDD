2b0fa1180859c06d1da65aff165dfc3756a3d6b4
rafaelsofizadeh <rafa.sofizadeh@gmail.com>
2021-02-16 01:51:04 +0400

    Address ValueObject
    Address was mistakenly defined as an Entity (Identifiable) instead of VO (StructurallyComparable)

--- src/order/domain/entity/Address.ts
+++ src/order/domain/entity/Address.ts
@@ -1,21 +1,17 @@
 import { IsISO31661Alpha3 } from 'class-validator';
-import { EntityProps } from '../../../common/domain/Entity';
-import { EntityId } from '../../../common/domain/EntityId';
-import { Identifiable } from '../../../common/domain/Identifiable';
+
+import { StructurallyComparable } from '../../../common/domain/StructurallyComparable';
 import { Validatable } from '../../../common/domain/Validatable';
 
-export class AddressProps extends EntityProps {
+export class AddressProps {
   @IsISO31661Alpha3()
   country: string;
 }
 
-export class Address extends Identifiable(Validatable(AddressProps)) {
-  constructor(
-    { id = new EntityId(), country }: AddressProps = new AddressProps(),
-  ) {
+export class Address extends StructurallyComparable(Validatable(AddressProps)) {
+  constructor({ country }: AddressProps = new AddressProps()) {
     super();
 
-    this.id = id;
     this.country = country;
   }
 }
