ae9908a7d0647d78c132594494d64654cccbe639
rafaelsofizadeh <rafa.sofizadeh@gmail.com>
2021-02-17 02:32:16 +0400

    Updated CustomerRepo interface

--- src/order/application/port/CustomerRepository.ts
+++ src/order/application/port/CustomerRepository.ts
@@ -3,5 +3,9 @@ import { Optional } from '../../../common/types';
 import { Customer } from '../../domain/entity/Customer';
 
 export abstract class CustomerRepository {
+  abstract addCustomer(customer: Customer): Promise<void>;
+
+  abstract deleteCustomer(customer: Customer): Promise<void>;
+
   abstract findCustomer(customerId: EntityId): Promise<Optional<Customer>>; // throws new Exception(Code.ENTITY_NOT_FOUND_ERROR, 'Customer not found.')
 }
