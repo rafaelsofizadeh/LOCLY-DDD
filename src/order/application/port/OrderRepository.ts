12423f6e68360fc82fa669d947f3b324fd32689d
rafaelsofizadeh <rafa.sofizadeh@gmail.com>
2021-02-17 01:31:45 +0400

    Customer-populated findOrder and findOrders repository methods

--- src/order/application/port/OrderRepository.ts
+++ src/order/application/port/OrderRepository.ts
@@ -4,5 +4,6 @@ import { Order } from '../../domain/entity/Order';
 export abstract class OrderRepository {
   abstract addOrder(order: Order): Promise<void>;
   abstract findOrder(orderId: EntityId): Promise<Order>;
+  abstract findOrders(orderIds: EntityId[]): Promise<Order[]>;
   abstract deleteOrder(orderId: EntityId): Promise<void>;
 }
--- src/order/infrastructure/repository/order/OrderMongoRepositoryAdapter.ts
+++ src/order/infrastructure/repository/order/OrderMongoRepositoryAdapter.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import * as MUUID from 'uuid-mongodb';
-import { Collection } from 'mongodb';
+import { Binary, Collection } from 'mongodb';
 import { InjectCollection } from 'nest-mongodb';
 
 import { EntityId } from '../../../../common/domain/EntityId';
@@ -15,6 +15,7 @@ import {
   mongoDocumentToOrder,
   OrderMongoDocument,
   orderToMongoDocument,
+  PopulatedOrderMongoDocument,
 } from './OrderMongoMapper';
 
 // TODO: mongoDocumentToXXX to a decorator
@@ -38,16 +39,16 @@ export class OrderMongoRepositoryAdapter implements OrderRepository {
     });
   }
 
+  // TODO: Should populate hosts too
   async findOrder(orderId: EntityId): Promise<Order> {
-    const orderDocument: OrderMongoDocument = await this.orderCollection.findOne(
-      { _id: MUUID.from(orderId.value) },
-    );
+    const {
+      customerId,
+      ...sanitizedOrderDocument
+    }: OrderMongoDocument = await this.orderCollection.findOne({
+      _id: MUUID.from(orderId.value),
+    });
 
-    const customer: Customer = await this.customerRepository.findCustomer(
-      muuidToEntityId(orderDocument.customerId),
-    );
-
-    if (!orderDocument) {
+    if (!sanitizedOrderDocument) {
       throw new Exception(
         Code.ENTITY_NOT_FOUND_ERROR,
         `Order (id: ${orderId.value}) not found`,
@@ -55,7 +56,63 @@ export class OrderMongoRepositoryAdapter implements OrderRepository {
       );
     }
 
-    return mongoDocumentToOrder({ ...orderDocument, customer });
+    const orderCustomer: Customer = await this.customerRepository.findCustomer(
+      muuidToEntityId(customerId),
+    );
+
+    return mongoDocumentToOrder({
+      ...sanitizedOrderDocument,
+      customer: orderCustomer,
+    });
+  }
+
+  // TODO: Should populate hosts too
+  async findOrders(orderIds: EntityId[]): Promise<Order[]> {
+    const orderMUUIDs: Binary[] = orderIds.map(({ value }) =>
+      MUUID.from(value),
+    );
+
+    const orderDocuments: OrderMongoDocument[] = await this.orderCollection
+      .find({ _id: { $in: orderMUUIDs } })
+      .toArray();
+
+    // TODO(NOW): ERROR HERE, in createHosts too
+    if (orderDocuments.length !== orderIds.length) {
+      const failedOrderIds: EntityId[] = orderIds.filter(
+        orderId =>
+          orderDocuments.findIndex(
+            orderDocument => MUUID.from(orderId.value) === orderDocument._id,
+          ) === -1,
+      );
+
+      throw new Exception(
+        Code.ENTITY_NOT_FOUND_ERROR,
+        `Orders (ids: ${failedOrderIds
+          .map(({ value }) => value)
+          .join(', ')}) not found`,
+        { orderIds, failedOrderIds },
+      );
+    }
+
+    const populatedOrderDocuments: PopulatedOrderMongoDocument[] = await Promise.all(
+      orderDocuments.map(async orderDocument => {
+        const orderCustomer: Customer = await this.customerRepository.findCustomer(
+          muuidToEntityId(orderDocument.customerId),
+        );
+
+        const { customerId, ...sanitizedOrderDocument } = orderDocument;
+
+        return {
+          ...sanitizedOrderDocument,
+          customer: orderCustomer,
+        };
+      }),
+    );
+
+    return populatedOrderDocuments.map(populatedOrderDocument =>
+      mongoDocumentToOrder(populatedOrderDocument),
+    );
+  }
 
   async deleteOrder(orderId: EntityId): Promise<void> {
     this.orderCollection.deleteOne({ _id: MUUID.from(orderId.value) });
