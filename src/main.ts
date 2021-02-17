4699915bf7f95aa977b9a7697577e10130a59a68
rafaelsofizadeh <rafa.sofizadeh@gmail.com>
2021-02-18 00:04:35 +0400

    Global TODO

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { AppModule } from './AppModule';
 // TODO(GLOBAL): default values in constructor or in parameters?
 // TODO(GLOBAL): https://stackoverflow.com/a/37300663/6539857 !!!
 
+/**
+TOOD(GLOBAL):
+Introduce polymorphism to the code, remove the “status” property, make every status a separate subclass
+Use mixins and composition to form the classes
+Move service functionality to the Domain Entities themselves
+*/
+
 declare const module: any;
 
 async function bootstrap() {
