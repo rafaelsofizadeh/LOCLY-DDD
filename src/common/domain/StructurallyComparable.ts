be34c55d9c3423fd348f9a138d49b82cb9e96a97
rafaelsofizadeh <rafa.sofizadeh@gmail.com>
2021-04-19 04:18:11 +0400

    Remove redundant mixins

--- src/common/domain/StructurallyComparable.ts
+++
@@ -1,9 +0,0 @@
-export function StructurallyComparable<C extends new (...args: any[]) => {}>(
-  Base: C,
-) {
-  return class StructurallyComparable extends Base {
-    equals(valueObject: {}) {
-      return JSON.stringify(this) === JSON.stringify(valueObject);
-    }
-  };
-}
