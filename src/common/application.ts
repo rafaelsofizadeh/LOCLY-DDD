b9a9e85eb789a292f7b2ab4fa6bbe7fc322cf0fa
rafaelsofizadeh <rafa.sofizadeh@gmail.com>
2021-04-27 21:51:52 +0400

    Inner function wrapper for withTransaction to abort transaction on error automatically

--- src/common/application.ts
+++ src/common/application.ts
@@ -1,19 +1,22 @@
 import { ClientSession, MongoClient } from 'mongodb';
+import { Exception } from './error-handling';
 
 export async function withTransaction<T>(
   fn: (session: ClientSession) => Promise<T>,
   mongoClient: MongoClient,
   session?: ClientSession,
 ): Promise<T> {
+  const wrappedFn = (session: ClientSession) =>
+    abortTransactionOnNonMongoException(session, fn);
   // Session takes precendence over mongoClient
   return session === undefined
-    ? await withNewSessionTransaction(fn, mongoClient)
-    : await withExistingSessionTransaction(fn, session);
+    ? await withNewSessionTransaction(mongoClient, wrappedFn)
+    : await withExistingSessionTransaction(session, wrappedFn);
 }
 
 async function withExistingSessionTransaction<T>(
-  fn: (session: ClientSession) => Promise<T>,
   session: ClientSession,
+  fn: (session: ClientSession) => Promise<T>,
 ): Promise<T> {
   // Session already in transaction, program will assume it's already wrapped in session.withTransaction
   if (session.inTransaction()) {
@@ -23,28 +26,47 @@ async function withExistingSessionTransaction<T>(
     return fn(session);
   }
 
-  try {
-    let result: T;
-    await session.withTransaction(async () => (result = await fn(session)));
-    return result;
-  } catch (error) {
-    console.log('Transaction aborted.');
-    throw error;
-  } finally {
-    await session.endSession({});
-  }
+  let result: T;
+  await session.withTransaction(async () => (result = await fn(session)));
+  return result;
 }
 
 async function withNewSessionTransaction<T>(
-  fn: (session: ClientSession) => Promise<T>,
   mongoClient: MongoClient,
+  fn: (session: ClientSession) => Promise<T>,
 ): Promise<T> {
   let result: T;
 
   await mongoClient.withSession(
     async (session: ClientSession) =>
-      await session.withTransaction(async () => (result = await fn(session))),
+      await session.withTransaction(
+        async (transactionalSession: ClientSession) =>
+          (result = await fn(transactionalSession)),
+      ),
   );
 
   return result;
+}
+
+async function abortTransactionOnNonMongoException<T>(
+  session: ClientSession,
+  fn: (session: ClientSession) => Promise<T>,
+): Promise<T> {
+  // Test if session has a transaction initialized (to abort that transaction)
+  if (!session.inTransaction()) {
+    throw new Error(
+      "Can't abort transaction as session isn't in transaction state",
+    );
+  }
+
+  try {
+    return await fn(session);
+  } catch (exceptionOrMongoError) {
+    if (exceptionOrMongoError instanceof Exception) {
+      await session.abortTransaction();
+      console.log('Transaction aborted');
+    }
+
+    throw exceptionOrMongoError;
+  }
 }
