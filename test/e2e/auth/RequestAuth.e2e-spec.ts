b740d34e61e737282ef2fcc3f52e5d464c3f04dc
rafaelsofizadeh <rafa.sofizadeh@gmail.com>
2021-06-06 01:18:16 +0400

    RequestAuth Host tests

--- test/e2e/auth/RequestAuth.e2e-spec.ts
+++ test/e2e/auth/RequestAuth.e2e-spec.ts
@@ -8,6 +8,8 @@ import { ICustomerRepository } from '../../../src/customer/persistence/ICustomer
 import { IHostRepository } from '../../../src/host/persistence/IHostRepository';
 import { EntityType } from '../../../src/auth/entity/Token';
 import { IEmailService } from '../../../src/infrastructure/email/IEmailService';
+import { originCountriesAvailable } from '../../../src/calculator/data/PriceGuide';
+import { Country } from '../../../src/order/entity/Country';
 
 // TODO(GLOBAL)(TESTING): Substitute database name in tests
 
@@ -106,6 +108,73 @@ describe('[POST /auth] IRequestAuth', () => {
           false,
         ),
       ).toMatchObject(oldCustomer);
+    });
+  });
+
+  describe('Host', async () => {
+    const hostEmail = 'testhost@requestauth.com';
+
+    beforeAll(() => {
+      // Email functionality isn't important to us right now
+      jest.spyOn(emailService, 'sendEmail').mockImplementation(async () => {});
+    });
+
+    afterAll(() =>
+      Promise.all([hostRepository.deleteHost({ email: hostEmail })]),
+    );
+
+    it('New', async () => {
+      expect(
+        await hostRepository.findHost({ email: hostEmail }, undefined, false),
+      ).toBeUndefined();
+
+      const hostCountry: Country = originCountriesAvailable[0];
+
+      const authUrl: string = await requestAuth.execute({
+        email: hostEmail,
+        type: EntityType.Host,
+        country: hostCountry,
+      });
+
+      expect(authUrl).toMatch(/localhost:3000\/auth\/.+/);
+
+      const newHost = await hostRepository.findHost(
+        { email: hostEmail },
+        undefined,
+        false,
+      );
+
+      expect(newHost).toMatchObject({
+        email: hostEmail,
+        country: hostCountry,
+        verified: false,
+        available: false,
+        profileComplete: false,
+      });
+    });
+
+    // IMPORTANT: Needs to run sequentially after 'New'
+    it('Existing', async () => {
+      const oldHost = await hostRepository.findHost(
+        { email: hostEmail },
+        undefined,
+        false,
+      );
+
+      expect(oldHost).toBeDefined();
+
+      const authUrl: string = await requestAuth.execute({
+        email: hostEmail,
+        type: EntityType.Host,
+      });
+
+      expect(authUrl).toMatch(/localhost:3000\/auth\/.+/);
+
+      // Nothing should change in the host object if it has already been registered — should be identical with
+      // oldHost
+      expect(
+        await hostRepository.findHost({ email: hostEmail }, undefined, false),
+      ).toMatchObject(oldHost);
     });
   });
 });
