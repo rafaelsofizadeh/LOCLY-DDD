51cdc3a8c6d83f859c30d589d2615595f2df6251
rafaelsofizadeh <rafa.sofizadeh@gmail.com>
2021-07-14 18:30:30 +0400

    createTestHost

--- test/e2e/utilities.ts
+++ test/e2e/utilities.ts
@@ -1,17 +1,23 @@
 import { TestingModule } from '@nestjs/testing';
-import { SuperAgentTest, agent } from 'supertest';
+import { SuperAgentTest, agent, Response } from 'supertest';
 import { IRequestAuth } from '../../src/auth/application/RequestAuth/IRequestAuth';
-import { EntityType } from '../../src/auth/entity/Token';
+import { UserType } from '../../src/auth/entity/Token';
 import {
   getDestinationCountriesAvailable,
   originCountriesAvailable,
 } from '../../src/calculator/data/PriceGuide';
-import { Address, Email } from '../../src/common/domain';
+import { Address, Email, UUID } from '../../src/common/domain';
 import { ICreateCustomer } from '../../src/customer/application/CreateCustomer/ICreateCustomer';
 import { IDeleteCustomer } from '../../src/customer/application/DeleteCustomer/IDeleteCustomer';
 import { IEditCustomer } from '../../src/customer/application/EditCustomer/IEditCustomer';
 import { IGetCustomer } from '../../src/customer/application/GetCustomer/IGetCustomer';
 import { Customer } from '../../src/customer/entity/Customer';
+import { ICreateHost } from '../../src/host/application/CreateHost/ICreateHost';
+import { IDeleteHost } from '../../src/host/application/DeleteHost/IDeleteHost';
+import { IEditHost } from '../../src/host/application/EditHost/IEditHost';
+import { IGetHost } from '../../src/host/application/GetHost/IGetHost';
+import { Host } from '../../src/host/entity/Host';
+import { IHostRepository } from '../../src/host/persistence/IHostRepository';
 import { Country } from '../../src/order/entity/Country';
 
 export async function createTestCustomer(
@@ -33,17 +39,15 @@ export async function createTestCustomer(
     IDeleteCustomer,
   );
 
-  const customerCountry: Country = getDestinationCountriesAvailable(
+  const email = `${UUID()}@email.com`;
+  const country: Country = getDestinationCountriesAvailable(
     orderOriginCountry,
   )[0];
-
   const address: Address = {
     addressLine1: '42 Random St.',
     locality: 'Random City',
-    country: customerCountry,
+    country: country,
   };
-
-  const email = 'random@email.com';
 
   const { id: customerId } = await createCustomer.execute({
     port: { email },
@@ -66,6 +70,67 @@ export async function createTestCustomer(
     editCustomer,
     getCustomer,
     deleteCustomer,
+  };
+}
+
+export async function createTestHost(
+  moduleRef: TestingModule,
+  orderOriginCountry: Country = originCountriesAvailable[0],
+): Promise<{
+  host: Host;
+  createHost: ICreateHost;
+  editHost: IEditHost;
+  getHost: IGetHost;
+  deleteHost: IDeleteHost;
+}> {
+  const createHost: ICreateHost = await moduleRef.resolve(ICreateHost);
+  const editHost: IEditHost = await moduleRef.resolve(IEditHost);
+  const getHost: IGetHost = await moduleRef.resolve(IGetHost);
+  const deleteHost: IDeleteHost = await moduleRef.resolve(IDeleteHost);
+
+  const hostRepository: IHostRepository = await moduleRef.resolve(
+    IHostRepository,
+  );
+
+  const email = `${UUID()}@email.com`;
+  const country: Country = orderOriginCountry;
+
+  const { id: hostId } = await createHost.execute({
+    port: { email, country },
+  });
+
+  const firstName = 'Host';
+  const lastName = 'Testov';
+  const address: Address = {
+    addressLine1: '42 Random St.',
+    locality: 'Random City',
+    country,
+  };
+
+  await editHost.execute({
+    port: {
+      currentHostProperties: { id: hostId },
+      firstName,
+      lastName,
+      address,
+    },
+  });
+
+  await hostRepository.setProperties(
+    { hostId },
+    { available: true, verified: true },
+  );
+
+  const host: Host = await getHost.execute({
+    port: { hostId, email },
+  });
+
+  return {
+    host,
+    createHost,
+    editHost,
+    getHost,
+    deleteHost,
   };
 }
 
