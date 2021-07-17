ff3d5e7eb2552d712b607b0d3bad12ad01dd68af
rafaelsofizadeh <rafa.sofizadeh@gmail.com>
2021-07-17 17:13:09 +0400

    Script changes

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,10 @@ if [[ $STATUS == *"nothing to commit, working tree clean"* ]]
 then
     npm run build
     sed -i "" '/dist/d' ./.gitignore
+    sed -i "" '/.env/d' ./.gitignore
+    cp Procfile dist/Procfile
+    cp package.deployment.json dist/package.json
+    cp .env dist/.env
     git add .
     git commit -m "Edit .gitignore to publish"
     # https://stackoverflow.com/a/65733058/6539857
