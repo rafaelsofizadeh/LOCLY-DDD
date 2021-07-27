285e84f23bc18d9f5cf68d7e24f95d2682abca0d
rafaelsofizadeh <rafa.sofizadeh@gmail.com>
2021-07-27 21:03:01 +0400

    Poland calculator

--- src/calculator/data/PLN/translation.js
+++ src/calculator/data/PLN/translation.js
@@ -10,53 +10,57 @@ const translate = new Fuse(translation, {
   keys: ['name'],
 });
 
-readFile(path.join(__dirname, './poland-data.csv'), 'utf8', (err, data) => {
-  if (err) throw err;
+readFile(
+  path.join(__dirname, './poland-zone-data.csv'),
+  'utf8',
+  (err, data) => {
+    if (err) throw err;
 
-  writeFile(
-    path.join(__dirname, './zones.json'),
-    JSON.stringify(
-      data
-        .split('\n')
-        .map(r => {
-          const [n, name, eu, pu] = r.split(',');
-          const e = eu.trim();
-          const p = pu.trim();
+    writeFile(
+      path.join(__dirname, './zones.json'),
+      JSON.stringify(
+        data
+          .split('\n')
+          .map(r => {
+            const [n, name, eu, pu] = r.split(',');
+            const e = eu.trim();
+            const p = pu.trim();
 
-          const matches = translate.search(name);
+            const matches = translate.search(name);
 
-          if (matches.length === 0) return undefined;
+            if (matches.length === 0) return undefined;
 
-          return [matches[0].item['iso2'], e, p];
-        })
-        .filter(Boolean)
-        .reduce(
-          (result, [iso2, e, p]) => {
-            const iso3 = alpha2ToAlpha3(iso2);
+            return [matches[0].item['iso2'], e, p];
+          })
+          .filter(Boolean)
+          .reduce(
+            (result, [iso2, e, p]) => {
+              const iso3 = alpha2ToAlpha3(iso2);
 
-            if (e) {
-              result.economy[e] = result.economy[e] || [];
-              result.economy[e].push(iso3);
-            }
+              if (e) {
+                result.economy[e] = result.economy[e] || [];
+                result.economy[e].push(iso3);
+              }
 
-            if (p) {
-              result.priority[p] = result.priority[p] || [];
-              result.priority[p].push(iso3);
-            }
+              if (p) {
+                result.priority[p] = result.priority[p] || [];
+                result.priority[p].push(iso3);
+              }
 
-            return result;
-          },
-          {
-            economy: {},
-            priority: {},
-          },
-        ),
-      null,
-      2,
-    ),
-    err => {
-      if (err) throw err;
-      console.log('Successful write.');
-    },
-  );
-});
+              return result;
+            },
+            {
+              economy: {},
+              priority: {},
+            },
+          ),
+        null,
+        2,
+      ),
+      err => {
+        if (err) throw err;
+        console.log('Successful write.');
+      },
+    );
+  },
+);
