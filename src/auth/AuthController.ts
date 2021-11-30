061e99505f7bae87ba55ab2661cb36545fe4dcaa
Rafael Sofi-zada <rafa.sofizadeh@gmail.com>
2021-12-01 02:16:21 +0400

    cookie configs

--- src/auth/AuthController.ts
+++ src/auth/AuthController.ts
@@ -19,7 +19,8 @@ import { COOKIE_CORS_CONFIG } from '../GlobalModule';
 
 @Controller('auth')
 export class AuthController {
-  private cookieConfig: CookieOptions;
+  private tokenCookieConfig: CookieOptions;
+  private authCookieConfig: CookieOptions;
 
   constructor(
     private readonly configService: ConfigService,
@@ -28,14 +29,15 @@ export class AuthController {
     @Inject(COOKIE_CORS_CONFIG)
     private readonly cookieCorsConfig: Partial<CookieOptions>,
   ) {
-    const authCookieMaxAge = ms(
-      this.configService.get<string>('AUTH_TOKEN_EXPIRES_IN'),
-    );
+    const maxAge = ms(this.configService.get<string>('AUTH_TOKEN_EXPIRES_IN'));
 
-    this.cookieConfig = {
-      maxAge: authCookieMaxAge,
+    const cookieConfig = {
+      maxAge,
       ...this.cookieCorsConfig,
     };
+
+    this.authCookieConfig = { ...cookieConfig, httpOnly: false };
+    this.tokenCookieConfig = { ...cookieConfig, httpOnly: true };
   }
 
   /**
@@ -62,27 +64,19 @@ export class AuthController {
     @Res({ passthrough: true }) response: Response,
     @VerificationTokenIdentity() verificationToken: Token,
   ): Promise<void> {
-    const authTokenString: string = this.verifyAuth.execute(verificationToken);
-    const authCookieName = this.configService.get<string>('TOKEN_COOKIE_NAME');
+    const tokenString: string = this.verifyAuth.execute(verificationToken);
+    const tokenCookieName = this.configService.get<string>('TOKEN_COOKIE_NAME');
 
     // Newly created auth token gets signed and reset in request cookies in place of the old (verification) token
     // cookie. This auth token lets the user subsequently repeatedly authorize requests. User is logged in.
-    response.cookie(authCookieName, authTokenString, {
-      ...this.cookieConfig,
-      httpOnly: true,
-    });
+    response.cookie(tokenCookieName, tokenString, this.tokenCookieConfig);
 
     // The auth token is an httpOnly cookie, hence there's no straightforward way for the front-end JS to
     // check whether the user is logged in or not. We add a second, client-accessible cookie, indicating the
     // auth status of the user (auth=true or auth=false), that doesn't participate in any back-end logic.
-    const authIndicatorCookieName = this.configService.get<string>(
-      'AUTH_INDICATOR_COOKIE_NAME',
-    );
+    const authCookieName = this.configService.get<string>('AUTH_I_COOKIE_NAME');
 
-    response.cookie(authIndicatorCookieName, true, {
-      ...this.cookieConfig,
-      httpOnly: false,
-    });
+    response.cookie(authCookieName, true, this.authCookieConfig);
 
     return response.redirect('https://locly.netlify.app/auth/success');
   }
@@ -93,18 +87,17 @@ export class AuthController {
     @Res({ passthrough: true }) response: Response,
     @AnyEntityIdentity() identity: Host | UUID,
   ): Promise<void> {
-    const authCookieName = this.configService.get<string>('TOKEN_COOKIE_NAME');
+    const tokenCookieName = this.configService.get<string>('TOKEN_COOKIE_NAME');
     // https://expressjs.com/en/4x/api.html#res.clearCookie
     // Web browsers and other compliant clients will only clear the cookie if the given options is identical to those
     // given to res.cookie(), excluding expires and maxAge.
-    response.clearCookie(authCookieName, this.cookieConfig);
+    response.clearCookie(tokenCookieName, this.tokenCookieConfig);
 
     const authIndicatorCookieName = this.configService.get<string>(
       'AUTH_INDICATOR_COOKIE_NAME',
     );
     response.cookie(authIndicatorCookieName, false, {
-      ...this.cookieCorsConfig,
-      httpOnly: false,
+      ...this.authCookieConfig,
       // 10 years
       maxAge: 365 * 24 * 60 * 60 * 10,
     });
